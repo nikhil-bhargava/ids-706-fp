c082ba6a8bb8958a207f0f22471b29dbfab5c3ff
Nikhil Bhargava <nikhil.bhargava.15@utexas.edu>
2020-11-20 11:59:56 -0500

    Adding flask app & additional requirements

---
+++ resale_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, session, redirect
+import os
+from pathlib import Path
+import pandas as pd
+app = Flask(__name__)
+
+repo_path = Path(os.getcwd())
+df = pd.read_csv(repo_path / 'data' / '07_model_output' / 'output.csv')
+df = df.rename(columns={'style_code':'Style Code', 'name': 'Sneaker Name', 'date':'Date', 
+                        'retail_price':'Retail', 'pred_resale_price':'Predicted Resale'})
+
+@app.route('/', methods=("POST", "GET"))
+def sneaker_table():
+    return render_template('resale_template.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+    # return render_template("resale_template2.html", column_names=df.columns.values, row_data=list(df.values.tolist()),link_column="Style Code", zip=zip)
+
+if __name__ == '__main__':
+    app.run()
