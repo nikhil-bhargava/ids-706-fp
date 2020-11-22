cfff1c8faa3d9aa32a9ea71f670c6e68b22f4aeb
Nikhil Bhargava <nikhil.bhargava.15@utexas.edu>
2020-11-22 23:25:53 +0000

    Making data loading changes to app

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ from pathlib import Path
 import pandas as pd
 app = Flask(__name__)
 
-repo_path = Path(os.getcwd())
-df = pd.read_csv(repo_path / 'data' / '07_model_output' / 'output.csv')
-df = df.rename(columns={'style_code':'Style Code', 'name': 'Sneaker Name', 'date':'Date', 
-                        'retail_price':'Retail', 'pred_resale_price':'Predicted Resale'})
-
 @app.route('/', methods=("POST", "GET"))
 def sneaker_table():
+    repo_path = Path(os.getcwd())
+    df = pd.read_csv(repo_path / 'data' / '07_model_output' / 'output.csv')
+    df = df.rename(columns={'style_code':'Style Code', 'name': 'Sneaker Name', 'date':'Date', 
+                            'retail_price':'Retail', 'pred_resale_price':'Predicted Resale'})
+
     return render_template('resale_template.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
     # return render_template("resale_template2.html", column_names=df.columns.values, row_data=list(df.values.tolist()),link_column="Style Code", zip=zip)
-
-@app.route('/')
-def hello():
-    return "hello"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
